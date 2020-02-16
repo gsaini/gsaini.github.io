8198f8ac969f53de9b7c6deaece3288e4783c8e0
Gopal Saini <gopal.saini.work@gmail.com>
2020-02-16 17:11:47 -0500

    Added Education Details

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,30 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import Timeline from './Components/Timeline';
 import Profile from './Components/Profile';
+import Education from './Components/Education';
 import Footer from './Components/Footer';
 import PROFILE from './data/gsaini-profile.json'
 import "./styles/app.scss";
 
 const App = () => {
-  const { title, company, designation, name, aboutMe, emailAddress, mobileNumber, avatarUrl, location, tiles } = PROFILE;
+  const { title, company, designation, name, aboutMe, emailAddress, mobileNumber, avatarUrl, location, tiles, educations } = PROFILE;
   const profileDetails = { name, company, designation, aboutMe, emailAddress, mobileNumber, avatarUrl, location };
 
   return (
     <div className="app-container">
       <Helmet>
-          <meta
-            name="description"
-            content={title}
-          />
-          <link rel="icon" href={avatarUrl}/>
-          <title>{title}</title>
+        <meta
+          name="description"
+          content={title}
+        />
+        <link rel="icon" href={avatarUrl} />
+        <title>{title}</title>
       </Helmet>
       <Profile {...profileDetails} />
       {
-        Object.entries(tiles).map(([key, tiles]) => <Timeline key={`tile-${key}`} {...tiles}/>)
+        Object.entries(tiles).map(([key, tiles]) => <Timeline key={`tile-${key}`} {...tiles} />)
       }
-
+      <Education {...{ educations: educations }} />
       <Footer />
     </div>
 
---
+++ src/Components/Education/Education.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import './_Education.scss'
+
+interface EducationState {
+    iconCls: string
+    year: string
+    institute: string
+    stream: string
+}
+
+interface IProps {
+    educations: Array<EducationState>
+}
+
+export const Education: React.FC<IProps> = ({ educations }) => {
+    return (
+        <section className="tile tile-education">
+            <div className="tile-content">
+                {
+                    educations.map(({ iconCls, year, institute, stream }) => {
+                        return (
+                            <div className="tile-card">
+                                <i className={iconCls}></i>
+                                <div className="card-education">
+                                    <h3>{institute}</h3>
+                                    <p>{stream}</p>
+                                    <p>{year}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </section>
+    );
+};
