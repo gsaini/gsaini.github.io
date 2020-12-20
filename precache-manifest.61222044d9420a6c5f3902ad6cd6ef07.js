bfb2ecff4be7d73674fb96dbc97f532406152c7b
Gopal Saini <gopal.saini.work@gmail.com>
2020-12-20 10:11:08 -0500

    Updates

---
+++ precache-manifest.61222044d9420a6c5f3902ad6cd6ef07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543dc940ab91bedbf419d72f2e137e46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef433fd93ac654c4e9e5",
+    "url": "/static/css/main.d1fa99be.chunk.css"
+  },
+  {
+    "revision": "b578768af05284bdaba7",
+    "url": "/static/js/2.c1de8bbe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c1de8bbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef433fd93ac654c4e9e5",
+    "url": "/static/js/main.c5ddfbce.chunk.js"
+  },
+  {
+    "revision": "de516b6f04d9c2f84886",
+    "url": "/static/js/runtime-main.fdf9dd77.js"
+  }
+]);
